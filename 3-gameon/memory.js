2c0fbd357fa2f627d899d6b3f56280ca58c52535
Tommy Nguyen <tn222eb@student.lnu.se>
2013-12-08 10:30:56 -0500

    Gjort metoden för att rita ut tabellen med bilderna

--- 3-gameon/memory.js
+++ 3-gameon/memory.js
@@ -1,5 +1,6 @@
 "use strict";
 
+// Variabler för antal kolumner och rader i tabellen
 var columns = 4;
 var rows = 4;
 
@@ -18,11 +19,43 @@ console.log(Memory.randomArray);
 Memory.generateTable();
 },
 
+// Ritar ut en tabellen med bilderna
+generateTable: function(){
+    
+// Skapa en table
+var table = document.createElement("table");
 
-generateTable: function() {
+// Lägger sedan i den i memoryTable
+document.getElementById("memoryTable").appendChild(table);
+
+// For-loopar antal kolumner och rader för tabellen
+ for (var row = 0; row < rows; row++){
+            var tr = document.createElement("tr");
+            // Lägger i de for-loopade tr-taggarna i table
+            table.appendChild(tr);
+            
+            for (var col = 0; col < columns; col++){
+                var td = document.createElement("td");
+                // Lägger i de for-loopade td-taggarna i tr-taggarna
+                tr.appendChild(td);
+                
+                // Skapar img-tagg och ger den frågeteckens bilden
+                var img = document.createElement("img");
+                img.setAttribute("src", "pics/0.png");
+                
+                // Skapar a-tagg
+                var a = document.createElement("a");
+                a.setAttribute("href", "#");
+                // Sätter i img-taggen i a-taggen
+                a.appendChild(img);
+                // Sätter i a-taggen i td-taggen
+                td.appendChild(a);
+                
+                                      }
+                                      }
     
 }
-    
+
 };
 
 window.onload = Memory.init;
