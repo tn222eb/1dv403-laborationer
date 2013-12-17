0b348159d3a437e8c1322bd6957b8151fc5c3de2
Tommy Nguyen <tn222eb@student.lnu.se>
2013-12-16 20:17:18 -0500

    Fixat lite

--- 4-forms/validering.js
+++ 4-forms/validering.js
@@ -1,35 +1,41 @@
 "use strict";
 
+
 var Validator = {
 
 // Egenskaper som jag kommer använda för att kontrollera fälten innan skicka
 first: false,
 last: false,
+post: false,
 mail: false,
-postal: false,
-price: false,
 
-init: function() {
+
+init: function () {
 
 // Hämtar ut de saker som finns i formuläret och sätter i variabeler
 var form = document.getElementById("form");
 var sendButton = document.getElementById("button");
 var fieldSet = document.getElementById("fieldset");
+
 var firstName = document.getElementById("firstname");
 var lastName = document.getElementById("lastname");
 var postalCode = document.getElementById("postalcode");
-var eMail = document.getElementById("email"); 
+var eMail = document.getElementById("email");
 
 // Funktion firstName som kommer validera när användaren tryck bort från rutan 
 firstName.onblur = function () {
 
 // Kollar om det är whitespaces
-if (!form.elements["firstname"].value.match(/\S/)){
+if (!form.elements["firstname"].value.match(/\S/))
+{
 // Ändrar elementets klass
 form.elements["firstname"].className = "error";
 // Ser till att formuläret inte skickas
 Validator.first = false;
 
+
+if (!document.getElementById("first").hasChildNodes())
+{
 // Skapar div-tagg
 var div = document.createElement("div");
 div.className = "errormessage";
@@ -40,24 +46,38 @@ document.getElementById("first").appendChild(div);
 var text = document.createTextNode("Detta fält får ej lämnas blankt!");
 // Lägger i text i div
 div.appendChild(text);
-                                           }
+
+}
+
+}
+
 else 
 {
 form.elements["firstname"].className = "valid";
 Validator.first = true;
+var element = document.getElementById("first");
+while (element.firstChild)
+{
+element.removeChild(element.firstChild);
 }
-                
-                               };
-                               
-// Funktion lastName som kommer validera när användaren tryck bort från rutan             
-lastName.onblur = function (){
+
+}
+};
+
+// Funktion lastName som kommer validera när användaren tryck bort från rutan   
+lastName.onblur = function () {
+
 // Kollar om det är whitespaces
-if (!form.elements["lastname"].value.match(/\S/)){
+if (!form.elements["lastname"].value.match(/\S/))
+{
 // Ändrar elementets klass
 form.elements["lastname"].className = "error";
 // Ser till att formuläret inte skickas
 Validator.last = false;
 
+
+if (!document.getElementById("last").hasChildNodes()) 
+{
 // Skapar div-tagg
 var div = document.createElement("div");
 div.className = "errormessage";
@@ -68,26 +88,37 @@ document.getElementById("last").appendChild(div);
 var text = document.createTextNode("Detta fält får ej lämnas blankt!");
 // Lägger i text i div
 div.appendChild(text);
-                                                 }
-                                                 
-else 
+}
+
+}
+
+else
 {
 form.elements["lastname"].className = "valid";
+Validator.last = true;
+var element = document.getElementById("last");
+while (element.firstChild) {
+element.removeChild(element.firstChild);
 }
-                              }; 
-                              
-postalCode.onblur = function (){
-    
-}; 
-                              
-eMail.onblur = function (){
-    
+
+}
 };
 
+postalCode.onblur = function () {
+
+}
+
+
+eMail.onblur = function () {
+
+}
+
 
 
 }
-    
+
+
+
 };
 
 window.onload = Validator.init;
