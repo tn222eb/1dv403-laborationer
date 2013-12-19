3d34dde9f37b2c9187e5067303fa88b6dfb442f8
Tommy Nguyen <tn222eb@student.lnu.se>
2013-12-19 08:57:25 -0500

    Klar med validering

--- 4-forms/validering.js
+++ 4-forms/validering.js
@@ -25,8 +25,8 @@ var eMail = document.getElementById("email");
 // Funktion firstName som kommer validera när användaren tryck bort från rutan 
 firstName.onblur = function () {
 
-// Kollar om det är whitespaces
-if (!form.elements["firstname"].value.match(/\S/))
+// Kollar om det är något annat än a-zåäö
+if (!form.elements["firstname"].value.match(/^[a-zåäö]+$/i))
 {
 // Ändrar elementets klass
 form.elements["firstname"].className = "error";
@@ -43,7 +43,7 @@ div.className = "errormessage";
 document.getElementById("first").appendChild(div);
 
 // Skapar text till div-taggen
-var text = document.createTextNode("Detta fält får ej lämnas blankt!");
+var text = document.createTextNode("Fältet får ej lämnas blankt");
 // Lägger i text i div
 div.appendChild(text);
 
@@ -53,7 +53,9 @@ div.appendChild(text);
 
 else 
 {
+// Ger klassen valid som i sin tur gör rutan grön    
 form.elements["firstname"].className = "valid";
+// Ser till att formuläret skickas
 Validator.first = true;
 var element = document.getElementById("first");
 while (element.firstChild)
@@ -67,10 +69,10 @@ element.removeChild(element.firstChild);
 // Funktion lastName som kommer validera när användaren tryck bort från rutan   
 lastName.onblur = function () {
 
-// Kollar om det är whitespaces
-if (!form.elements["lastname"].value.match(/\S/))
+// Kollar om det är något annat än a-zåäö
+if (!form.elements["lastname"].value.match(/^[a-zåäö]+$/i))
 {
-// Ändrar elementets klass
+// Ger klassen error som i sin tur gör rutan röd
 form.elements["lastname"].className = "error";
 // Ser till att formuläret inte skickas
 Validator.last = false;
@@ -85,7 +87,7 @@ div.className = "errormessage";
 document.getElementById("last").appendChild(div);
 
 // Skapar text till div-taggen
-var text = document.createTextNode("Detta fält får ej lämnas blankt!");
+var text = document.createTextNode("Fältet får ej lämnas blankt");
 // Lägger i text i div
 div.appendChild(text);
 }
@@ -94,6 +96,7 @@ div.appendChild(text);
 
 else
 {
+// Ger klassen valid som i sin tur gör rutan grön   
 form.elements["lastname"].className = "valid";
 Validator.last = true;
 var element = document.getElementById("last");
@@ -104,16 +107,169 @@ element.removeChild(element.firstChild);
 }
 };
 
-postalCode.onblur = function () {
+postalCode.onblur = function (){
+
+if(form.elements["postalcode"].value.match(/^(SE)?[\ ]?[0-9]{3}(-|\ )?[0-9]{2}$/))
+{
+form.elements["postalcode"].value = form.elements["postalcode"].value.replace(/(SE|\ |-)/g, '');
+// Ger klassen valid som i sin tur gör rutan grön  
+form.elements["postalcode"].className = "valid";
+// Ser till att formuläret skickas
+Validator.postal = true;
+var element = document.getElementById("postal");
+while (element.firstChild) {
+element.removeChild(element.firstChild);
+}
+}
+
+else
+{
+// Ger klassen error som i sin tur gör rutan röd    
+form.elements["postalcode"].className = "error"; 
+Validator.postal = false;
+
+if (!document.getElementById("postal").hasChildNodes()) {
+var div = document.createElement("div");
+div.className = "errormessage";
+document.getElementById("postal").appendChild(div);
+
+var text = document.createTextNode("Fyll i ett korrekt postnummer");
+div.appendChild(text)
+
 
 }
 
+}
+},
 
-eMail.onblur = function () {
+
+eMail.onblur = function (){
+
+if (!form.elements["email"].value.match(/^[\w]+(\.[\w]+)*@([\w]+\.)+[a-z]{2,7}$/i)) {
+// Ger klassen error som i sin tur gör rutan röd    
+form.elements["email"].className = "error";
+Validator.mail = false;
+
+if (!document.getElementById("mail").hasChildNodes()) {
+var div = document.createElement("div");
+div.className = "errormessage";
+document.getElementById("mail").appendChild(div);
+
+var text = document.createTextNode("Fyll i en korrekt e-postadress");
+div.appendChild(text);
+}
 
 }
 
+else 
+{
+// Ger klassen valid som i sin tur gör rutan grön      
+form.elements["email"].className = "valid";
+// Ser till att formuläret inte skickas
+Validator.mail = true;
+var element = document.getElementById("mail");
+while (element.firstChild) {
+element.removeChild(element.firstChild);
+                           }
+}  
 
+};
+
+// Skicka funktion
+sendButton.onclick = function () {
+// Ifall någon av dessa är false så kommer det inte gå skickas
+if ((Validator.first === false)||(Validator.last === false)||(Validator.postal === false)||(Validator.mail === false)) 
+{
+return false;
+}
+
+// Skapar div-tagg för att använda som skuggbakgrund
+var background = document.createElement("div");
+background.className = "background";
+// Sätter i den i body
+document.body.appendChild(background);
+
+// Skapar en annan div-tagg som kommer användas för popup fönster
+var div = document.createElement("div");
+div.className = "popupWindow";
+
+document.body.appendChild(div);
+
+var h3 = document.createElement("h3");
+var header = document.createTextNode("Vänligen bekräfta ditt köp!");
+
+div.appendChild(h3);
+h3.appendChild(header);
+
+var p = document.createElement("p");
+var fname = document.createTextNode (form.elements["firstname"].name + ": " + form.elements["firstname"].value);
+p.appendChild(fname);
+div.appendChild(p);
+
+p = document.createElement("p");
+var lname = document.createTextNode (form.elements["lastname"].name + ": " + form.elements["lastname"].value);
+p.appendChild(lname);
+div.appendChild(p);
+
+p = document.createElement("p");
+var postnumber = document.createTextNode (form.elements["postalcode"].name + ": " + form.elements["postalcode"].value);
+p.appendChild(postnumber);
+div.appendChild(p);
+
+p = document.createElement("p");
+var epost = document.createTextNode (form.elements["email"].name + ": " + form.elements["email"].value);
+p.appendChild(epost);
+div.appendChild(p);
+
+p = document.createElement("p");
+var price = document.createTextNode (form.elements["price"].name + ": " + form.elements["price"].value);
+p.appendChild(price);
+div.appendChild(p);
+
+var closeButton = document.createElement("input");
+closeButton.type = "button";
+closeButton.value = "Ändra";
+div.appendChild(closeButton);
+
+closeButton.onclick = function() {
+// Har på fältet i formuläret
+form.elements["firstname"].disabled = false;
+form.elements["lastname"].disabled = false;
+form.elements["postalcode"].disabled = false;
+form.elements["email"].disabled = false;
+form.elements["price"].disabled = false;
+form.elements["button"].disabled = false;
+// Tar bort popup fönster och skuggbakgrund
+document.body.removeChild(div);
+document.body.removeChild(background);
+}; 
+
+var sendButton = document.createElement("input");
+sendButton.type = "submit";
+sendButton.value = "Bekräfta ditt köp";
+div.appendChild(sendButton);
+
+sendButton.onclick = function() {
+form.elements["firstname"].disabled = false;
+form.elements["lastname"].disabled = false;
+form.elements["postalcode"].disabled = false;
+form.elements["email"].disabled = false;
+form.elements["price"].disabled = false;
+form.elements["button"].disabled = false;
+form.submit();
+}; 
+
+// Avaktiverar fälten i formuläret
+form.elements["firstname"].disabled = true;
+form.elements["lastname"].disabled = true;
+form.elements["postalcode"].disabled = true;
+form.elements["price"].disabled = true;
+form.elements["email"].disabled = true;
+form.elements["button"].disabled = true;
+
+return false;
+
+};
 
 }
 
